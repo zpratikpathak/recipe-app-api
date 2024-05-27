a1f9583c70ee790add108424e53a381088973502
Pratik Pathak <pratikpathak200@gmail.com>
2024-05-27 10:39:11 +0530

    addedreceipe model

--- app/core/models.py
+++ app/core/models.py
@@ -35,3 +35,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    # Recipe Object
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
